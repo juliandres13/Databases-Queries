cf7e7657abdb5ac89b35c54278e4c1e026617aa8
Julián XIX <juliandresleon13@gmail.com>
2023-12-10 10:14:36 -0500

    avances 2.1

--- ConduExpert_NoSQL.js
+++ ConduExpert_NoSQL.js
@@ -1,12 +1,15 @@
-use condu_expert
-
-db.createCollection('academias')
-db.createCollection('instructores')
-db.createCollection('estudiantes')
-db.estudiantes.aggregate([{$group: {_id: '$cedula'}}])
-db.createCollection('')
-db.createCollection('')
-db.createCollection('')
-db.createCollection('')
-db.createCollection('')
-db.createCollection('')
+// Para todas las consultas
+
+use condu_expert
+
+db.createCollection('academias')
+
+db.createCollection('instructores')
+
+db.createCollection('estudiantes')
+
+db.createCollection('salones')
+
+db.createCollection('vehiculos')
+
+db.estudiantes.aggregate([{$group: {_id: '$cedula'}}])
