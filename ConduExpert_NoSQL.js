78077ce7c45e402bfc2661767af878aebcef0f58
Julián XIX <juliandresleon13@gmail.com>
2023-12-10 09:53:34 -0500

    avances 2

---
+++ ConduExpert_NoSQL.js
@@ -0,0 +1,12 @@
+use condu_expert
+
+db.createCollection('academias')
+db.createCollection('instructores')
+db.createCollection('estudiantes')
+db.estudiantes.aggregate([{$group: {_id: '$cedula'}}])
+db.createCollection('')
+db.createCollection('')
+db.createCollection('')
+db.createCollection('')
+db.createCollection('')
+db.createCollection('')
---
+++ README.md
@@ -0,0 +1,2 @@
+# Para importar colecciones:
+mongoimport --db tu_base_de_datos --collection tu_coleccion --drop --file tu_ruta_del_archivo_json 
